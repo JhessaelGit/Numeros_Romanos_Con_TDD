301532829b01325209e6d8e37ebbb330e16a98b2
Jhessael Siancas <jhessael.siancas@ucb.edu.bo>
2025-02-20 12:54:45 -0400

    Feat: Ahora convierte el 2 en Romano II

--- src/NumerosARomanos.js
+++ src/NumerosARomanos.js
@@ -1,6 +1,9 @@
 function ConvertirARomanos(numero)
 {
-    return "I";
+    if(numero === 1)
+        return "I";
+    else if(numero === 2)
+        return "II";
 }
 
 export default ConvertirARomanos;
--- src/NumerosARomanos.test.js
+++ src/NumerosARomanos.test.js
@@ -3,4 +3,7 @@ describe("Convertir a Numeros Romanos", () => {
     it("Deberia convetir el 1 en I", () => {
         expect(ConvertirARomanos(1)).toEqual("I");
     })
+    it("Deberia convetir el 2 en II", () => {
+        expect(ConvertirARomanos(2)).toEqual("II");
+    })
 })
