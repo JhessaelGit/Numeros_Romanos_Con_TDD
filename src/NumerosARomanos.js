147a4dcb952193b20404d6bf3d8278b043ecbdf1
Jhessael Siancas <jhessael.siancas@ucb.edu.bo>
2025-02-25 09:34:45 -0400

    Refactoring: el codigo se redujo y se hizo mas censillo y no tan repetitivo se aplico en los numeros del 1 al 99

--- src/NumerosARomanos.js
+++ src/NumerosARomanos.js
@@ -1,28 +1,27 @@
+function numerosRomanos(numero,Romano_1,Romano_5,Romano_10){
+  let numeroRomano = "";
+  if (numero === 1) numeroRomano = Romano_1;
+  else if (numero === 2) numeroRomano = Romano_1 + Romano_1;
+  else if (numero === 3) numeroRomano = Romano_1 + Romano_1 + Romano_1;
+  else if (numero === 4) numeroRomano = Romano_1 + Romano_5;
+  else if (numero === 5) numeroRomano = Romano_5;
+  else if (numero === 6) numeroRomano = Romano_5 + Romano_1;
+  else if (numero === 7) numeroRomano = Romano_5 + Romano_1 + Romano_1;
+  else if (numero === 8) numeroRomano = Romano_5 + Romano_1 + Romano_1 + Romano_1;
+  else if (numero === 9) numeroRomano = Romano_1 + Romano_10;
+  return numeroRomano;
+}
+
+
 function NumerosDel1Al9(numero) {
   let numeroRomano = "";
-  if (numero === 1) numeroRomano = "I";
-  else if (numero === 2) numeroRomano = "II";
-  else if (numero === 3) numeroRomano = "III";
-  else if (numero === 4) numeroRomano = "IV";
-  else if (numero === 5) numeroRomano = "V";
-  else if (numero === 6) numeroRomano = "VI";
-  else if (numero === 7) numeroRomano = "VII";
-  else if (numero === 8) numeroRomano = "VIII";
-  else if (numero === 9) numeroRomano = "IX";
+  numeroRomano=numerosRomanos(numero,"I","V","X");
   return numeroRomano;
 }
 
 function NumerosDel10Al90(numero) {
   let numeroRomano = "";
-  if (numero >= 10 && numero < 20) numeroRomano = "X";
-  else if (numero >= 20 && numero < 30) numeroRomano = "XX";
-  else if (numero >= 30 && numero < 40) numeroRomano = "XXX";
-  else if (numero >= 40 && numero < 50) numeroRomano = "XL";
-  else if (numero >= 50 && numero < 60) numeroRomano = "L";
-  else if (numero >= 60 && numero < 70) numeroRomano = "LX";
-  else if (numero >= 70 && numero < 80) numeroRomano = "LXX";
-  else if (numero >= 80 && numero < 90) numeroRomano = "LXXX";
-  else if (numero >= 90) numeroRomano = "XC";
+  numeroRomano=numerosRomanos(numero,"X","L","C");
   return numeroRomano;
 }
 
@@ -46,7 +45,7 @@ function ConvertirARomanos(numero) {
   if (numero >= 1 && numero <= 9) {
     numeroRomano = NumerosDel1Al9(numero);
   } else if (numero >= 10 && numero <= 90) {
-    numeroRomano = NumerosDel10Al90(numero) + NumerosDel1Al9(numero % 10);
+    numeroRomano = NumerosDel10Al90(Math.floor(numero/10)) + NumerosDel1Al9(numero % 10);
   }
   else if (numero >= 100 && numero <= 900) {
     numeroRomano = NumerosDel100Al900(numero) + NumerosDel10Al90(numero % 100) + NumerosDel1Al9(numero % 10);
